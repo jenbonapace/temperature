4c417732363879a417b1d592b14399b6c875ffc7
JenBonapace <you@example.com>
2017-02-02 14:06:44 -0500

    temp

---
+++ script.js
@@ -0,0 +1,13 @@
+window.alert("hi")
+
+
+var Farenheit = prompt("Enter a temperature in Degrees Farenheit");
+var Celsius;
+
+
+Celsius=(Farenheit-32)/(9/5);
+Farenheit=(Celsius*9/5+32);
+
+console.log("The temperature in Celsius is " + Celsius + " degrees");
+prompt(If you would like to conver to Degrees Farenheit from Celsius)
+console.log("The temperature in Farenheit is " + Farenheit + " degrees");
