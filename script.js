f19d0bb109fa28958382240c6b49e547cb920d0e
jenbonapace <jennifer.bonapace@gmail.com>
2017-02-07 07:32:09 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ var Farenheit = prompt("Enter a temperature in Degrees Farenheit");
 var Celsius;
 
 
-Celsius=(Farenheit-32)/(9/5);
-Farenheit=(Celsius*9/5+32);
+Celsius = (Farenheit - 32) / (9 / 5);
+Farenheit = (Celsius * 9 / 5 + 32);
 
 console.log("The temperature in Celsius is " + Celsius + " degrees");
 prompt(If you would like to conver to Degrees Farenheit from Celsius)
---
+++ script1.js
@@ -0,0 +1,24 @@
+window.alert("hi")
+
+
+
+/*var temperature=prompt("Enter a temperature you wish to convert");
+var
+var F=(temperature*9/5+32);
+var C=((temperature*9/5+32)-32)/(9/5);*/
+
+/*need to figure this out using a string and  number, how is this any different than the other day?*/
+
+
+var=C "degrees in celsius"
+var=temperature 'degrees in Farenheit'
+
+
+
+function convertTemp (temperature*9/5+32);
+else (temperature*9/5+32)-32)/(9/5);
+if
+
+/*this is much harder than I thought. wtf am i doing?
+ convertTemp(212, "C"); // > 100   convertTemp(32, "C");  // > 0
+   convertTemp(65, "C"); // > 18.333   convertTemp(0, "F"); // > 32 */
